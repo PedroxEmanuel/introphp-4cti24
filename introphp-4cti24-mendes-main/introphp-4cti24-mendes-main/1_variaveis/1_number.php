ca8424409e083f98c2385777529c25322a1d3f50
Felipe Satox <125518285+FelipeSatoX@users.noreply.github.com>
2024-03-13 22:10:44 -0300

    Add files via upload

---
+++ introphp-4cti24-mendes-main/introphp-4cti24-mendes-main/1_variaveis/1_number.php
@@ -0,0 +1,10 @@
+<?php 
+    /*variáveis */
+    $idade = 17; //inteiro
+    $nome = "Felipe Sato"; //string
+    $estuda = null; // booleano
+    $salario = 0.00;
+    echo 'Olá me chamo ' . $nome . ' e tenho ' . $idade . ' anos';
+
+    echo 'Estudo: ' . $estuda . ' Salário ' . $salario;
+?>
---
+++ introphp-4cti24-mendes-main/introphp-4cti24-mendes-main/1_variaveis/2_operadore.php
@@ -0,0 +1,18 @@
+<?php
+
+  $a = 24;
+  $b = 16;
+
+  $d= $a * $b;
+  echo ' <br> A soma é ' . $d;
+
+  $e = $a - $b;
+  echo ' <br>A multiplicacao é ' . $e;
+
+  $f = $a / $b;
+  echo ' <br> A divisao é' . $f;
+
+
+  $g = $a % $b;
+  echo ' <br> O módulo é' . $g;
+?>  
